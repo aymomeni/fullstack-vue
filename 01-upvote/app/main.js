12f0ea8e98908e34f73e2f35452d7918bb904f0d
Ali Momeni <aymomeni@gmail.com>
2019-05-28 17:15:18 -0600

    added javascript computed sortedSubmissions function to main.js, now articles are sorted by the number of votes

--- 01-upvote/app/main.js
+++ 01-upvote/app/main.js
@@ -3,6 +3,13 @@ new Vue({
     el: '#app',
     data: {
         submissions: Seed.submissions
+    },
+    computed:{
+        sortedSubmissions(){
+            return this.submissions.sort((a,b) => {
+                return b.votes - a.votes
+            });
+        }
     }
 });
 
